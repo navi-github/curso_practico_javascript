9206addb8187bf8f7b0fdb5be809a17d515a4ed9
Ivan <hp_692c@hotmail.com>
2021-08-04 05:16:52 -0500

    Se agrega taller #2, precios y descuentos

--- README.md
+++ README.md
@@ -9,3 +9,10 @@ Proyecto del Curso Práctico de JavaScript en PLATZI
 - Segundo paso: implementar las fórmulas en JavaScript
 - Tercer paso: crear funciones
 - Cuarto paso: integrar JavaScript con HTML
+
+##  Taller #2: Porcentajes y Descuentos
+
+- Primer paso: definir las fórmulas
+- Segundo paso: implementar las fórmulas en JavaScript
+- Tercer paso: crear funciones
+- Cuarto paso: integrar JavaScript con HTML
---
+++ descuentos.js
@@ -0,0 +1,19 @@
+function calcularPrecioConDescuento(precio, descuento) {
+    const porcentajePrecioConDescuento = 100 - descuento;
+    const precioConDescuento = (precio * porcentajePrecioConDescuento) / 100;
+
+    return precioConDescuento;
+}
+
+function onClickButtonPriceDiscount() {
+    const inputPrice = document.getElementById("inputPrice");
+    const inputDiscount = document.getElementById("inputDiscount");
+    const priceValue = inputPrice.value;
+    const discountValue = inputDiscount.value;
+
+    const precioConDescuento = calcularPrecioConDescuento(priceValue, discountValue);
+
+    const resultP = document.getElementById("resultPrice");
+    resultP.innerText = "El Precio con Descuento son $ " + precioConDescuento;
+}
+
